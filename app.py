319690054247971bfb5e9123e425a68a81b1f043
KarthickPandiyaraj <karthick961998@gmail.com>
2022-10-22 02:18:03 +0530

    add 404,500 pages and errorhandler code

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+#Create custom error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Create internal server error page
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 @app.route('/')
 def index():
